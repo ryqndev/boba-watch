77bd826f14398d909e24f97b6ee38e7ebed1beda
Ryan Yang <javathon@gmail.com>
2019-03-30 08:30:24 -0700

    Updates

---
+++ precache-manifest.734621505672155d47fd9f5e2e60ba1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "159cb70db403f895953d",
+    "url": "/boba-watch/static/css/main.ef1ca56d.chunk.css"
+  },
+  {
+    "revision": "159cb70db403f895953d",
+    "url": "/boba-watch/static/js/main.99cef860.chunk.js"
+  },
+  {
+    "revision": "cb868e7effdbfb348013",
+    "url": "/boba-watch/static/js/runtime~main.fb5d26fc.js"
+  },
+  {
+    "revision": "6a3e7554b4218be1228c",
+    "url": "/boba-watch/static/js/2.bfd11a1f.chunk.js"
+  },
+  {
+    "revision": "6e37ab8797c64fc1149121da543f81d5",
+    "url": "/boba-watch/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.561147f2965be8a7e8a5ef9e35820f7c.js"
+  "/boba-watch/precache-manifest.734621505672155d47fd9f5e2e60ba1a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/boba-watch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

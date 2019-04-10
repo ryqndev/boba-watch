fd75e1aea3c8a36e9f98cd77cacdc7ddae5afdbd
Ryan Yang <javathon@gmail.com>
2019-04-10 11:20:17 -0700

    Updates

--- precache-manifest.3645880288787c965b34a3f40e68d22d.js
+++ precache-manifest.3645880288787c965b34a3f40e68d22d.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "b7f5cd49167bd172199d",
+    "revision": "4a7e743b77b2fbdcb679",
     "url": "/static/css/main.2b816d8c.chunk.css"
   },
   {
-    "revision": "b7f5cd49167bd172199d",
-    "url": "/static/js/main.02151fc6.chunk.js"
+    "revision": "4a7e743b77b2fbdcb679",
+    "url": "/static/js/main.93bc7499.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "7885e043413e9e837964",
+    "revision": "e50a1c13152413606938",
     "url": "/static/css/2.d995f49c.chunk.css"
   },
   {
-    "revision": "7885e043413e9e837964",
-    "url": "/static/js/2.d10b56de.chunk.js"
+    "revision": "e50a1c13152413606938",
+    "url": "/static/js/2.f53ecaae.chunk.js"
   },
   {
     "revision": "609a27caffd0313d87d0465178adcc8a",
     "url": "/static/media/logo.609a27ca.svg"
   },
   {
-    "revision": "086c84bf4f98874f0092a53b05f51783",
+    "revision": "fc261990ac7ae76ec0f3725c595b46de",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6280b3cfb6b0d80494c4ee5dfaae1e1f.js"
+  "/precache-manifest.3645880288787c965b34a3f40e68d22d.js"
 );
 
 workbox.clientsClaim();

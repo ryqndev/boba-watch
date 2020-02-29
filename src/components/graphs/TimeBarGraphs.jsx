ba45f0574a7ad4dcd0b67e995d3c67d9eae8f691
Ryan Yang <ryanqyang@gmail.com>
2020-02-28 18:35:13 -0800

    converted to hooks

--- src/components/graphs/TimeBarGraphs.jsx
+++ src/components/graphs/TimeBarGraphs.jsx
@@ -1,62 +1,57 @@
-import React, { Component } from 'react';
-import {Card} from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
 import { XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries, DiscreteColorLegend } from 'react-vis';
 import 'react-vis/dist/style.css';
 import './styles/timebargraphs.css'
 
 let shortName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-export class TimeBarGraphs extends Component {
-    constructor(props){
-        super(props);
-        this.state = this.parse(this.props.data);
-    }
-    parse = (data) => {
-        let bd = { morn: [], noon: [] }
+const TimeBarGraphs = ({width, data}) => {
+    const [morning, setMorning] = useState([]);
+    const [noon, setNoon] = useState([]);
+
+    useEffect(() => {
+        let m = [];
+        let n = [];
         data.forEach((day, i) => {
             let morn = 0, noon = 0;
             day.forEach((quantityAtTime, j) => {
                 j >= 11 ? noon += quantityAtTime : morn += quantityAtTime;
             });
-            bd.morn.push({x: shortName[i], y: morn});
-            bd.noon.push({x: shortName[i], y: noon});
-            
+            m.push({x: shortName[i], y: morn});
+            n.push({x: shortName[i], y: noon});
+            setMorning(m);
+            setNoon(n);
         });
-        return bd;
-    }
-    render() {
-        const s = this.state;
-        return (
-            <Card className="time-bar-graph--holder">
-                <XYPlot width={this.props.width - 40} height={300} stackBy="y" xType="ordinal">
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis title={'# of purchases'}/>
-                <VerticalBarSeries color={'#FFACAC'} data={s.morn} />
-                <VerticalBarSeries color={'#FFDCDC'} data={s.noon} />
-                </XYPlot>
-                <DiscreteColorLegend
-                    style={{ textAlign:'center'}}
-                    orientation="horizontal"
-                    items={
-                        [
-                            {
-                                title: 'Morning Purchases',
-                                color: '#FFACAC',
-                                strokeWidth: '5px'
-                            },
-                            {
-                                title: 'Afternoon Purchases',
-                                color: '#FFDCDC',
-                                strokeWidth: '5px'
-                            }
-                        ]
-                    }
-                />
-            </Card>
-        );
-    }
+    }, [data, setMorning, setNoon]);
+
+    return (
+        <div className="card time-bar-graph--holder">
+            <XYPlot width={width - 40} height={300} stackBy="y" xType="ordinal">
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis />
+            <YAxis title={'# of purchases'}/>
+            <VerticalBarSeries color={'#FFACAC'} data={morning} />
+            <VerticalBarSeries color={'#FFDCDC'} data={noon} />
+            </XYPlot>
+            <DiscreteColorLegend
+                style={{ textAlign:'center'}}
+                orientation="horizontal"
+                items={
+                    [{
+                        title: 'Morning Purchases',
+                        color: '#FFACAC',
+                        strokeWidth: 5
+                    },
+                    {
+                        title: 'Afternoon Purchases',
+                        color: '#FFDCDC',
+                        strokeWidth: 5
+                    }]
+                }
+            />
+        </div>
+    );
 }
 
 export default TimeBarGraphs;

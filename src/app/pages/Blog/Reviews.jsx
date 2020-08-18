19c21f4986ce9ba63985ff06d448a509b0cb78ac
Ryan Yang <ryanqyang@gmail.com>
2020-08-17 22:22:40 -0700

    Attaching functionality to increase number of posts displayed. May have an issue with rerendering certain components as some posts are flashing - look into later. Separated all css styles and put them in reviews section. Reviews component should now be done

--- src/app/pages/Blog/Reviews.jsx
+++ src/app/pages/Blog/Reviews.jsx
@@ -6,6 +6,7 @@ import {
     alertBlogPostDeletedSuccess,
     confirmBlogPostDelete
 } from '../../libs/swal';
+import './Reviews.scss';
 
 const Reviews = ({ownerUID, currentUID, initialDisplayCount=5}) => {
     const [posts, setPosts] = useState(null);
@@ -36,11 +37,12 @@ const Reviews = ({ownerUID, currentUID, initialDisplayCount=5}) => {
             }
         })
     }
+    const displayMore = () => { setDisplayCount(displayCount + 5) }
     const ContentDisplay = (posts) => {
         if(!posts)              return (<h3 key="@ryqndev/loading">Loading...</h3>);
         if(posts.length === 0)  return (<h3 key="@ryqndev/empty">No Published Reviews</h3>);
         
-        return posts.map(post => (
+        let content = posts.map(post => (
             <FeedItem key={post.id} place={post.location} {...post}>
                 {(ownerUID === currentUID) && (
                     <div className="item-controls">
@@ -48,10 +50,17 @@ const Reviews = ({ownerUID, currentUID, initialDisplayCount=5}) => {
                     </div>)
                 }
             </FeedItem>));
+
+        content.push(
+            posts.length >= displayCount 
+            ? <div key="@ryqndev/show" className="more" onClick={displayMore}>Click to show more</div>
+            : <div key="@ryqndev/end"className="end">No more posts to show.</div>
+        );
+        return content;
     }
 
     return (
-        <div className="content">
+        <div className="blog-reviews--wrapper">
             {ContentDisplay(posts)}
         </div>
     )

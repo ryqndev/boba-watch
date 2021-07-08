1c9b3a4000edea5e06d6abee53190b800db9c02b
Ryan Yang <ryanqyang@gmail.com>
2021-07-08 14:33:17 -0700

    custom made heatmap. Should be cleaned up later but its good for now

--- src/app/pages/Dashboard/DesktopDashboard/DesktopDashboard.jsx
+++ src/app/pages/Dashboard/DesktopDashboard/DesktopDashboard.jsx
@@ -1,16 +1,19 @@
-import { memo, useContext } from 'react';
+import { memo, useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import AuthUserContext from '../../../controller/contexts/AuthUserContext';
 import useDrinks from '../../../controller/hooks/useDrinks.js';
 import { Card, Searchbar } from '../../../components';
 import { Map, BudgetPieChart, PurchaseTimeHeatMap } from './components';
 import DrinkPanel from '../../History/DrinkPanel';
-import AuthUserContext from '../../../controller/contexts/AuthUserContext';
 import cn from './DesktopDashboard.module.scss';
 
 const DesktopDashboard = () => {
 	const { t } = useTranslation();
 	const { drinks } = useDrinks();
-    const [user] = useContext(AuthUserContext);
+	const [user] = useContext(AuthUserContext);
+	const [metrics, setMetrics] = useState(
+		JSON.parse(localStorage.getItem('metrics'))
+	);
 
 	return (
 		<div className={cn.wrapper}>
@@ -19,15 +22,25 @@ const DesktopDashboard = () => {
 					<h1 className={cn.title}>{t('dashboard')}</h1>
 				</header>
 				<div className={cn.content}>
-					<Card className={cn.map}><Map /></Card>
-					<Card className={cn.map}><PurchaseTimeHeatMap /></Card>
+					<Card className={cn.map}>
+						<Map />
+					</Card>
+					<Card className={cn.heatmap}>
+						<h2>Drink Frequency</h2>
+						<PurchaseTimeHeatMap data={metrics.d} />
+					</Card>
+					<div></div>
+					<div></div>
 				</div>
 			</main>
 			<aside className={cn.sidebar}>
 				<div className={cn.container}>
 					<Card className={cn.search}>
 						<h2>Monthly Budget</h2>
-						<BudgetPieChart budget={user.profile.budget} spent={JSON.parse(localStorage.getItem('metrics')).tc}/>
+						<BudgetPieChart
+							budget={user.profile.budget}
+							spent={metrics.tc}
+						/>
 					</Card>
 					<Card className={cn.search}>
 						<h2>Search</h2>
--- src/app/pages/Dashboard/DesktopDashboard/components/PurchaseTimeHeatMap/PurchaseTimeHeatMap.jsx
+++ src/app/pages/Dashboard/DesktopDashboard/components/PurchaseTimeHeatMap/PurchaseTimeHeatMap.jsx
@@ -1,8 +1,89 @@
 import { memo } from 'react';
+import { HeatmapRect } from '@visx/heatmap';
+import { Group } from '@visx/group';
+import { AxisTop, AxisLeft } from '@visx/axis';
+import { scaleBand } from '@visx/scale';
+import { Grid } from '@visx/grid';
 import cn from './PurchaseTimeHeatMap.module.scss';
+import { Translate } from '@material-ui/icons';
 
-const PurchaseTimeHeatMap = () => {
-	return <div className={cn.wrapper}>dfs</div>;
+const DATE_LABELS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const TIME_LABELS = [
+    "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 AM",
+    "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"
+];
+
+const PurchaseTimeHeatMap = ({ data }) => {
+	let max = 0;
+	const binData = data.map((e, i) => ({
+		bin: i + 1,
+		bins: e.map((e, i) => {
+			if(e > max) max = e;
+			return {
+				count: e,
+				bin: i,
+			};
+		})
+	}));
+	const xScale = e => {
+		return 26.5 * e + 4;
+	};
+	const yScale = e => {
+		return 16 * e + 12;
+	};
+
+	const colorScale = count =>
+		`rgba(246, 128, 128, ${count === 0 ? (max > 5 ? 0.01 : 0.1) : count / max})`;
+
+	return (
+		<svg className={cn.wrapper} viewBox='0 0 214 400'>
+			<AxisTop
+				scale={scaleBand({
+					domain: DATE_LABELS,
+					range: [24, 209]
+				})}
+				top={14}
+				tickLength={4}
+				tickClassName={cn.tick}
+				tickStroke='grey'
+				stroke={'#aaa'}
+				tickLabelProps={() => ({
+					className: cn['tick-label'],
+					y: -7,
+					textAnchor: 'middle',
+				})}
+			/>
+			<AxisLeft
+				scale={scaleBand({
+					domain: TIME_LABELS,
+					range: [13, 396]
+				})}
+				left={24}
+				orientation="left"
+				tickLength={4}
+				tickClassName={cn.tick}
+				tickStroke='grey'
+				stroke={'#aaa'}
+				numTicks={24}
+				tickLabelProps={() => ({
+					className: cn['tick-label'],
+					transform: 'translate(0, 2)',
+					x: -22,
+					textAnchor: 'left',
+				})}
+			/>
+			<Group left={20}>
+				<HeatmapRect
+					data={binData}
+					xScale={xScale}
+					yScale={yScale}
+					colorScale={colorScale}
+					binWidth={27.5}
+					binHeight={17}
+				></HeatmapRect>
+			</Group>
+		</svg>
+	);
 };
 
 export default memo(PurchaseTimeHeatMap);
